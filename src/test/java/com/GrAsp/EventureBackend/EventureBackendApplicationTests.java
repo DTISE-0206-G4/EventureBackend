4f5435ea4c6073475e2cac88f63a401c95f72888
goraasep <gora.asep@gmail.com>
2024-11-12 17:55:37 +0700

    chore(init): initial commit

---
+++ src/test/java/com/GrAsp/EventureBackend/EventureBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.GrAsp.EventureBackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventureBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
